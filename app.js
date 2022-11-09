faa82afab48aec49050184c3ccf844a966b159ff
Jinseo <2000jinseo@gmail.com>
2022-11-10 00:25:25 +0900

    [UPDATE] Saving and loading username

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAM = "hidden";
+const USERNAME_KEY = "key_username";
+
 
 function onLoginSubmit(event){
     event.preventDefault();         // 브라우저의 기본 동작을 막아줌 (여기서는 새로고침을 막기 위함)
     loginForm.classList.add(HIDDEN_CLASSNAM);
 
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGrettings(username);
+}
+
+
+function paintGrettings(username){
     greeting.innerText = `Hello ${username}!`;          // greeting.innerText = "Hello " + username;
     greeting.classList.remove(HIDDEN_CLASSNAM);
 }
 
 
-// Listener 안의 function은 내가 아닌 브라우저가 실행하는 것
-loginForm.addEventListener("submit", onLoginSubmit);    // submit 발생조건 : 엔터, 버튼 클릭
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAM);
+    loginForm.addEventListener("submit", onLoginSubmit);    // submit 발생조건 : 엔터, 버튼 클릭
+
+}else{
+    // show the greeting
+    paintGrettings(savedUsername);
+}
